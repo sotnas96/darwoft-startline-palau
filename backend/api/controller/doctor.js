acd7523bd5805bcc6c8cb8901856667bc5dcd8b7
sotnas96 <santosgonzalezpalau@gmail.com>
2024-04-26 16:54:50 -0400

    image uploads and validation

--- backend/api/controller/doctor.js
+++ backend/api/controller/doctor.js
@@ -1,59 +1,71 @@
-// const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 const { User, DoctorProfile } = require("../dbconfig");
+const getMedicAreas = require("../utils/getMedicAreas");
 
-const doctorController =
-{
-    getProfile: async (req, res) =>
-    {
-        try
-        {
+const getAreaId = async (value) => {
+    const areas = await getMedicAreas();
+    const medicArea = areas.find((obj) => obj['area'] === value);
+    return medicArea.id;
+}
+
+const doctorController = {
+    getProfile: async (req, res) => {
+        try {
             let payload = req.customData
             let doctorProfile = await DoctorProfile.findOne({user: payload.userId});
+            if (!doctorProfile) throw new Error('Invalid user');
             res.status(200).json({success: true, payload, doctorProfile}); 
-        }
-        catch(error)
-        {   
-            res.status(400).json({success: false, error});
+
+        } catch(error){   
+            res.status(400).json({success: false, error: error.message});
         }
     },
-    updateProfile: async (req, res) =>
-    {
-        try
-        {
-            //------------------------------------------------  doctor update validations missing -----------------------------
-            // let errors = validationResult(req);
-            // if (!errors.isEmpty())  throw new Error({success:false, errors: errors.array()});
+    updateProfile: async (req, res) => {
+        try {
+            if (!req.file) throw new Error('Please upload a .jpg, .png or .jpeg file');
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({success:false, errors: errors.array()});
+            }
             let userId = {user: req.customData.userId};
-            let doctorProfile = {...req.body};
+            const areaId = await getAreaId(req.body.area);
+            let doctorProfile = {
+                ...req.body,
+                area: areaId
+            };
             const profileUpdate = await DoctorProfile.findOneAndUpdate(
                 userId,
                 doctorProfile,
-                {
-                    returnOriginal: false
-                }
+                { returnOriginal: false }
             );
-            res.status(200).json({success: true, message: 'doctor updated', profileUpdate});
-        }
-        catch(error)
-        {
-            res.status(400).json({success: false, error, message: 'doctor controller update failed'});
+            res.status(200).json({
+                success: true, 
+                message: 'doctor updated', 
+                profileUpdate
+            });
+        } catch(error) {
+            res.status(400).json({
+                success: false, 
+                error: error.message, 
+                message: 'doctor controller update failed'
+            });
         }
     },
-    deleteProfile: async (req, res) =>
-    {
-        try
-        {
+    deleteProfile: async (req, res) => {
+        try {
             const userId = req.customData.userId;
             const delDocProfile = await DoctorProfile.deleteOne({user: userId});
             const delUser = await User.deleteOne({_id: userId});
             return res.status(200).json({
                 success: true, 
                 delDocProfile, 
-                delUser});
-        }
-        catch(error)
-        {
-            res.status(400).json({success: false, error});
+                delUser
+            });
+        } catch(error) {
+            res.status(400).json({
+                success: false, 
+                error: error.message
+            });
         }
     }
 };
--- backend/api/controller/patient.js
+++ backend/api/controller/patient.js
@@ -1,27 +1,24 @@
 const {validationResult} = require('express-validator');
 const { User, PatientProfile } = require("../dbconfig");
 
-const patientController =
-{
-    getProfile: async (req, res) =>
-    {
-        try
-        {
+const patientController = {
+    getProfile: async (req, res) => {
+        try {
             let payload = req.customData
             let patientProfile = await PatientProfile.findOne({user:payload.userId});
             res.status(200).json({success:true, payload, patientProfile}); 
-        }
-        catch(error)
-        {   
+
+        } catch(error) {   
             res.status(400).json({success:false, error});
         }
     },
-    updateProfile: async (req, res) =>
-    {
-        try
-        {
-            let errors = validationResult(req);
-            if (!errors.isEmpty())  throw new Error({success:false, errors: errors.array()});
+    updateProfile: async (req, res) => {
+        try {
+            if (!req.file) throw new Error('Please upload a .jpg, .png or .jpeg file');
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({success:false, errors: errors.array()});
+            }
             let userId = {user: req.customData.userId};
             let patientProfile = {...req.body};
             const profileUpdate = await PatientProfile.findOneAndUpdate(
@@ -32,10 +29,9 @@ const patientController =
                 }
             );
             res.status(200).json({success:true, message:'patient updated', profileUpdate});
-        }
-        catch(error)
-        {
-            res.status(400).json({success:false, error, message:'patient controller update failed'});
+
+        } catch(error) {
+            res.status(400).json({success:false, error: error.message});
         }
     },
     deleteProfile: async (req, res) =>
@@ -52,7 +48,7 @@ const patientController =
         }
         catch(error)
         {
-            res.status(400).json({success: false, error});
+            res.status(400).json({success: false, error: error.message});
         }
     }
 };
